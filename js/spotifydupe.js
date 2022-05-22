3d71b953047d7c6b5ba83998c5cac80c3642cabb
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2022-05-22 00:54:38 -0400

    update spotifydupe.js

--- js/spotifydupe.js
+++ js/spotifydupe.js
@@ -1,5 +1,5 @@
 function spotifyLogin() {//implement state and PKCE parameters
-  var url = "https://accounts.spotify.com/authorize";
+  let url = "https://accounts.spotify.com/authorize";
   url += "?client_id=" + "0c2ae7c6bfa04d98b164f6e30559c13d";
   url += "&response_type=code";
   url += "&redirect_uri=" + encodeURI("http://andonli.com/spotifydupe.html");
@@ -9,12 +9,46 @@ function spotifyLogin() {//implement state and PKCE parameters
   window.location.href = url;
 
 }
+// URL: http://andonli.com/spotifydupe.html?code=AQAGTBzz8bHRTSlfx940K0sPCUvBy-O8cAjKJ-1E-QL27J6PeHpITXtFUuKcOMqP5wReoau87rDphNEWQIzzp-gJtwGXaW0FBQvX6Cco75K1q1TD8UptsRHY66UaJsK-BOI2ZJFgxfRvGXNfDJ2m1wN8QNB48NA_VHQG9Mkr8X-gJUmP9dhOIy_6GVR5cHDh6AMqSLgtsMsIzD2g3ohSGfNC6Zd3HqI5nQjAfDq1W-oejpUKKgUu
+
+function onPageLoad() {
+  if (window.location.search.length > 0) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let code = urlParams.get('code');
+    console.log(code);
+    fetchAccessToken(code);
+  }
+}
+
+function fetchAccessToken(code) {
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code;
+  body += "&redirect_uri=" + encodeURI("http://andonli.com/spotifydupe.html");
+  console.log(body);
+  callAuthAPI(body);
+}
+
+function callAuthAPI(body) {
+  const Http = new XMLHttpRequest();
+  const url="https://accounts.spotify.com/api/token";
+  Http.open("POST", url);
+  Http.setRequestHeader("Authorization", "Basic " + bota("0c2ae7c6bfa04d98b164f6e30559c13d" + ":" + "72c948097a25423293ce6e1d49d8e070")); //how to hide secret client id? use pxce?
+  Http.setRequestHeader("Content_Type", "application/x-www-form-urlencoded");
+  Http.send(body);
+
+  Http.onreadystatechange=(e)=>{
+    if (Http.readyState == XMLHttpRequest.DONE) {
+      console.log(Http.responseText);
+
+    }
+  }
+}
+
+function handleAuthResponse() {
 
 
-
-
-
-
+}
 
 function getPlaylistList() {
   const spotifyAuth = document.getElementById('spotifyAuth').value
