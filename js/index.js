8c5e883dd33f8f686fdd44c2f8e311a504a623f5
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2022-06-13 12:47:18 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementsByClassName('header');
+var header = document.getElementById("myHeader");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
