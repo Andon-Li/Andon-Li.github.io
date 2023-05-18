eabc4d34b0aab0dbff1cf37115fb2df997c15191
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2023-05-18 17:08:31 -0500

    .

---
+++ js/voice.js
@@ -0,0 +1,9 @@
+function process_prompt() {
+  prompt = document.getElementById('prompt_input').value.toUpperCase();
+  prompt_array = prompt.split(' ');
+  console.log(prompt_array);
+}
+fetch('../cmudict-0.7b.txt')
+  .then(function(){
+    console.log('hello')
+  })
