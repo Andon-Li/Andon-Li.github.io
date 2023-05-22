ee6c7f6bb46016e6bff2f6c74cab2987403f512a
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2023-05-21 21:42:15 -0400

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -6,22 +6,22 @@ function process_prompt() {
   for (word of prompt_array) {
     switch (word.slice(-1)) {
       case '.':
-        var punctuation = 'PERIOD';
+        phonetic_list.push('PERIOD');
         word = word.slice(0, -1);
         console.log('a period has been found');
         break
       case ',':
-        var punctuation = 'COMMA';
+        phonetic_list.push('COMMA');
         word = word.slice(0, -1);
         console.log('a comma has been found');
         break
       case '!':
-        var punctuation = 'EXCLAIM';
+        phonetic_list.push('EXCLAIM');
         word = word.slice(0, -1);
         console.log('a excalim has been found');
         break
       case '?':
-        var punctuation = 'QUESTION';
+        phonetic_list.push('QUESTION');
         word = word.slice(0, -1);
         console.log('a question has been found');
         break
@@ -36,14 +36,10 @@ function process_prompt() {
     for (line of dictionary) {
       split = line.split('  ');
       if (split[0] == word) {
-        phonetic_list.push(split[1].split(' '));
-        if (punctuation) {
-          phonetic_list[phonetic_list.length - 1].push(punctuation);
-        }
+        phonetic_list[phonetic_list.length - 1].push(split[1].split(' '));
         break
       }
     }
-    
   }
   document.getElementById('text_area').innerHTML = phonetic_list
 }
