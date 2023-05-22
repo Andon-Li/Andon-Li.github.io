868d6fedbff3f2e09a7e06d347dcf8edd926ba80
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2023-05-21 21:46:13 -0400

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -5,33 +5,34 @@ function process_prompt() {
 
   for (word of prompt_array) {
     switch (word.slice(-1)) {
+
       case '.':
         phonetic_list.push('PERIOD');
         word = word.slice(0, -1);
         console.log('a period has been found');
         break
+
       case ',':
         phonetic_list.push('COMMA');
         word = word.slice(0, -1);
         console.log('a comma has been found');
         break
+
       case '!':
         phonetic_list.push('EXCLAIM');
         word = word.slice(0, -1);
         console.log('a excalim has been found');
         break
+
       case '?':
         phonetic_list.push('QUESTION');
         word = word.slice(0, -1);
         console.log('a question has been found');
         break
-      default:
-        var punctuation = 'NONE';
-    }
 
-    console.log(prompt_array);
-    console.log(punctuation);
-    console.log(word);
+      default:
+        phonetic_list.push('NONE');
+    }
 
     for (line of dictionary) {
       split = line.split('  ');
@@ -43,8 +44,10 @@ function process_prompt() {
   }
   document.getElementById('text_area').innerHTML = phonetic_list
 }
-var phonetic_list = []
+
+var phonetic_list;
 var dictionary;
+
 fetch('../cmudict-0.7b.txt')
   .then((response) => response.text())
   .then((text) => {
