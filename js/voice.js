35a0f357a64979d25f057535b8e3adf8a538fd87
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2023-05-24 20:15:40 -0400

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -84,7 +84,7 @@ function pronounce() {
       Y:  [36000,1000],
       Z:  [37000,1000],
       ZH: [38000,1000],
-      __default: [0, 1000]
+      __default: [0, 38000]
     }
   });
 
@@ -103,3 +103,16 @@ fetch('../cmudict-0.7b.txt')
   .then((text) => {
     dictionary = text.split('\n');
 });
+
+var sound = new Howl({
+  src: ['../assets/phonetics.mp3'],
+  sprite: {
+    blast: [0, 3000],
+    laser: [4000, 1000],
+    winner: [6000, 5000],
+    __default: [0, 1000]
+  }
+});
+
+// Shoot the laser!
+sound.play('laser');
