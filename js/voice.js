630885cea642f575afd1f293e77d4a816c257842
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2023-05-25 11:09:44 -0400

    .

--- js/voice.js
+++ js/voice.js
@@ -43,7 +43,7 @@ function process_prompt() {
 
 async function pronounce() {
   var sound = new Howl({
-    src: ['../assets/soundfile1.mp3'],
+    src: ['../assets/phonetics.mp3'],
     sprite: {
       AA0: [0,1000],
       AA1: [0,1000],
